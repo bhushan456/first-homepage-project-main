1b910e09bf5a6435d3f248e4cabec571d6dc436a
Himanshu8085 <himanshu.kothekar29@gmail.com>
2025-11-28 00:52:35 +0530

    2 c

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/first-homepage-project-main/",
   server: {
     host: "::",
     port: 8080,
